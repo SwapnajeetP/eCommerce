a18e4eade55d7c50335c3ca2115e4350624a5e68
SwapnajeetP <swapnajeetpawarsp@gmail.com>
2024-03-07 15:14:46 +0530

    added changed filter from buttons to checkbox

--- my-app/src/Components/Products.js
+++ my-app/src/Components/Products.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Productsstyle.css";
 
 export default function Products({ filterprod, handleAdd, filterItem }) {
+  const [home, setHome] = useState(true);
+  const [clothes, setClothes] = useState(true);
+  const [electronics, setElectronics] = useState(true);
+  const [decor, setDecor] = useState(true);
+  const [utility, setUtility] = useState(true);
+
+  const handleCheck = (data) => {
+    if (data === "home") {
+      if (home == true) {
+        filterItem(data);
+      } else {
+        setHome(false);
+      }
+    }
+
+    if (data === "clothes") {
+      if (clothes == true) {
+        filterItem(data);
+      } else {
+        setClothes(false);
+      }
+    }
+
+    if (data === "electronics") {
+      if (electronics == true) {
+        filterItem(data);
+      } else {
+        setElectronics(false);
+      }
+    }
+
+    if (data === "decor") {
+      if (decor == true) {
+        filterItem(data);
+      } else {
+        setDecor(false);
+      }
+    }
+
+    if (data === "utility") {
+      if (utility == true) {
+        filterItem(data);
+      } else {
+        setUtility(false);
+      }
+    }
+  };
+
   return (
     <>
-      <div className="catBtn">
+      {/* <div className="catBtn">
         <button
           className="btn btn-secondary filterbtn"
           onClick={() => filterItem("home")}
@@ -35,6 +83,38 @@ export default function Products({ filterprod, handleAdd, filterItem }) {
         >
           Utility
         </button>
+      </div> */}
+      <div className="leftProduct">
+        <input
+          type="checkbox"
+          value={home}
+          onChange={() => handleCheck("home")}
+        />{" "}
+        Home <br />
+        <input
+          type="checkbox"
+          value={clothes}
+          onChange={() => handleCheck("clothes")}
+        />{" "}
+        Clothes <br />
+        <input
+          type="checkbox"
+          value={electronics}
+          onChange={() => handleCheck("electronics")}
+        />{" "}
+        Electronics <br />
+        <input
+          type="checkbox"
+          value={decor}
+          onChange={() => handleCheck("decor")}
+        />{" "}
+        Decoration <br />
+        <input
+          type="checkbox"
+          value={utility}
+          onChange={() => handleCheck("utility")}
+        />{" "}
+        Utility <br />
       </div>
       <div className="shop-section">
         {filterprod.map((productItems) => (
